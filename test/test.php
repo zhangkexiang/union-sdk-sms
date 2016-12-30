ad3c94228672c02e2313a30bcb355682384929d1
zhangkexiang <59916375@qq.com>
2016-12-30 19:03:16 +0800

    update

--- test/test.php
+++ test/test.php
@@ -3,4 +3,4 @@ require __DIR__ . '/../vendor/autoload.php';
 
 use Union\Sdk\Sms\SmsEntrance;
 
-//SmsEntrance::send('captcha',"11111111","{\"ma\":\"111111\"}");
+SmsEntrance::send('captcha',"11111111","{\"ma\":\"111111\"}");
